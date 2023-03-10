3c6133d9ccf9eef35d25a48c4fa63a8dae4fa1f0
Shivam Samadhiya <127015282+shivam-samadhiya-01@users.noreply.github.com>
2023-03-10 19:44:29 +0530

    Add files via upload

---
+++ LinkedList/LinkedList1_.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+
+
+public class LinkedList1_{
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    
+    public static void main(String[] args) {
+        LinkedList1_ obj=new LinkedList1_();
+    }
+}
+
